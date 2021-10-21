c43430bcf9a11e0383188f4a1f018869f05204c7
Lewis Saunders <lewis-saunders@live.nmit.ac.nz>
2021-10-21 18:56:02 +1300

    style.css updated (image wasnt available, needed to use final code source) and app.js updated

--- users-app/app.js
+++ users-app/app.js
@@ -58,3 +58,46 @@ for (let i = 0, len = addUserInputsUI.length; i < len; i++) {
 usersRef.push(newUser, function () {
     console.log("data has been inserted");
 })
+
+// edit icon 
+let editIconUI = document.createElement("span");
+editIconUI.class = "edit-user";
+editIconUI.innerHTML = " ✎";
+editIconUI.setAttribute("userid", key);
+editIconUI.addEventListener("click", editButtonClicked) // Append after li.innerHTML = value.name 
+$li.append(editIconUI);
+
+// show the Edit User Form 
+document.getElementById('edit-user-module').style.display = "block";
+
+//set user id to the hidden input field 
+document.querySelector(".edit-userid").value = e.target.getAttribute("userid");
+
+const userRef = dbRef.child('users/' + e.target.getAttribute("userid"));
+
+// set data to the user field 
+const editUserInputsUI = document.querySelectorAll(".edit-user-input");
+
+userRef.on("value", snap => {
+    for (var i = 0, len = editUserInputsUI.length; i < len; i++) {
+        var key = editUserInputsUI[i].getAttribute("data-key");
+        editUserInputsUI[i].value = snap.val()[key];
+    }
+});
+
+const saveBtn = document.querySelector("#edit-user-btn"); 
+saveBtn.addEventListener("click", saveUserBtnClicked)
+
+const userID = document.querySelector(".edit-userid").value;
+
+const userRef = dbRef.child('users/' + userID);
+var editedUserObject = {}
+const editUserInputsUI = document.querySelectorAll(".edit-user-input");
+
+editUserInputsUI.forEach(function (textField) {
+    let key = textField.getAttribute("data-key");
+    let value = textField.value;
+    editedUserObject[textField.getAttribute("data-key")] = textField.value
+});
+
+document.getElementById('edit-user-module').style.display = "none";
