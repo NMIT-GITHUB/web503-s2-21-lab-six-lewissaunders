546dbda94f53d24d55f6de7116498c464ed35ad4
Lewis Saunders <lewis-saunders@live.nmit.ac.nz>
2021-10-21 15:27:45 +1300

    Updated app.js to have firebase config and init firebase

---
+++ users-app/app.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA9iHGfmxZCMn0GfNiBtecB8jV5bLQWR08",
+    authDomain: "tutorial-build-e34f1.firebaseapp.com",
+    projectId: "tutorial-build-e34f1",
+    storageBucket: "tutorial-build-e34f1.appspot.com",
+    messagingSenderId: "7442298986",
+    appId: "1:7442298986:web:36fb98a255f83583dd3213",
+    measurementId: "G-XVXTC5M3QZ"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
